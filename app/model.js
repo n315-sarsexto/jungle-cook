d1d4c7f7613a1e314999ffa424dccbfbfbfa11ea
Summer S <snowykittin@gmail.com>
2022-11-19 19:37:56 -0500

    started recipes.json

--- app/model.js
+++ app/model.js
@@ -1,5 +1,9 @@
+//import JSON as this is where data is modified
+import recipe from '../data/recipes.json' assert {type: 'json'}
+
 //basic change page function
 export function changePage(pageID, callback){
+    console.log(recipe)
     //if the page ID is nothing, go to the home page. otherwise, go to whatever page it is.
     if(pageID == ""){
         $.get(`pages/home/home.html`, function (data) {
