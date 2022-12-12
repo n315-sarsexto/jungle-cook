ea6a1e456d56a1435d8d0d8556b02b31650a8d07
Summer S <snowykittin@gmail.com>
2022-12-12 09:44:45 -0500

    revert change

--- app/model.js
+++ app/model.js
@@ -158,11 +158,11 @@ function addUserFunctions(id) {
   if(currentUser != null){
     $("#recipes #name-user").html(users[currentUser]["first-name"])
   }
-  $("#recipes #view-button").html(
+  $("#recipes #view-button").append(
     `<a href="#view/${id}"><button class="yellow-button">View</button></a>`
   );
   $("#recipes #user-buttons")
-    .html(`<a href="#edit/${id}"><button class="yellow-button user-button">Edit Recipe</button></a>
+    .append(`<a href="#edit/${id}"><button class="yellow-button user-button">Edit Recipe</button></a>
              <button id="${id}" class="yellow-button user-button delete">Delete</button>`);
 }
 
